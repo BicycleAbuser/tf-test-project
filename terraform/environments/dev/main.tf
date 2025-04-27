522489df858a31e11f56fe292aae8df5af4bf1ca
maxwell <maxwell@mail.com>
2025-04-27 13:18:23 +0300

    Modules typo fixes

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
 }
 
 module "storage_account" {
-  source              = "../../modules/storage_account"
+  source              = "../../modules/storage-account"
   storage_account_name = var.storage_account_name
   resource_group_name  = var.resource_group_name
   location             = var.location
--- terraform/environments/qa/main.tf
+++ terraform/environments/qa/main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
 }
 
 module "key_vault" {
-  source              = "../../modules/key_vault"
+  source              = "../../modules/key-vault"
   key_vault_name       = var.key_vault_name
   resource_group_name  = var.resource_group_name
   location             = var.location
