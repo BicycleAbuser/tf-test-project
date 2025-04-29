6fad96f41ec3ee3a67041dac2f819144c37fd463
maxwell <maxwell@mail.com>
2025-04-29 20:28:21 +0300

    TF: az CLI SP auth

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -9,3 +9,8 @@ variable "resource_group_name" {
 variable "location" {
   type = string
 }
+
+variable "client_id" {}
+variable "client_secret" {}
+variable "subscription_id" {}
+variable "tenant_id" {}
--- terraform/environments/qa/variables.tf
+++ terraform/environments/qa/variables.tf
@@ -13,3 +13,8 @@ variable "location" {
 variable "tenant_id" {
   type = string
 }
+
+variable "client_id" {}
+variable "client_secret" {}
+variable "subscription_id" {}
+variable "tenant_id" {}
